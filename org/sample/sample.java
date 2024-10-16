b8e3a734c1674077b2691ad335fa828061411e1a
VigneshKri <20ise0047@vvce.ac.in>
2024-10-16 21:09:13 +0530

    Commit message

--- org/sample/sample.java
+++ org/sample/sample.java
@@ -4,5 +4,6 @@ public class sample {
     public static void main(String[] args) {
         System.out.println("Repo is running ");
         System.out.println("edited Code..");
+        System.out.println("Git configured!!!");
     }
 }
