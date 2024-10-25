2ca5ec95a1b37a9aa9bae334970aff4feb071e71
VigneshKri <20ise0047@vvce.ac.in>
2024-10-25 18:02:02 +0530

    Added1

--- org/sample/sample1.java
+++ org/sample/sample1.java
@@ -4,4 +4,5 @@ public class sample1 {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
     }
+    // Adding a comment to demonstrate committing changes
 }
